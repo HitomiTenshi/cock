2b0faf344c41de201e8656d88e31f6e81bc06064
Johann Rekowski <johann.rekowski@gmail.com>
2022-01-22 20:03:20 +0100

    Significantly improve performance by using sharp instead of canvas and add some meaningful progress logs in the console.

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
-import {createCanvas, loadImage} from 'canvas';
+import {mkdir, readdir, readFile} from 'fs/promises';
 import _sizeOf from 'image-size';
 import {promisify} from 'util';
 import {parse} from 'yaml';
 import * as glob from 'fast-glob';
-import * as fs from 'fs/promises';
+import * as sharp from 'sharp';
 
-import {cartesian} from './lib';
+import {cartesian, LayerSize} from './lib';
 
 const sizeOf = promisify(_sizeOf);
+const BATCH_SIZE = 100;
 
 async function main() {
+  await mkdir('./output', {recursive: true});
   const layerDirs = await getLayerDirectories();
   const orderedDirs = await getLayerOrder();
   const missingDir = layerDirs.find(layerDir => !orderedDirs.includes(layerDir));
@@ -18,34 +20,20 @@ async function main() {
     console.error(
       `Cannot find layer directory "${missingDir}"! Check your config.yml file and add the correct directory names in the order that you need.`,
     );
+
     return;
   }
 
   const layerImages = await getLayerImages(orderedDirs);
   const layerSize = await getLayerSize(layerImages);
-  const imageCombinations = generateImageCombinations(layerImages);
-
-  for (let i = 0; i < imageCombinations.length; i++) {
-    const imageCombination = imageCombinations[i];
-    const canvas = createCanvas(layerSize.width, layerSize.height);
-    const ctx = canvas.getContext('2d');
-
-    for (const imagePath of imageCombination) {
-      const image = await loadImage(imagePath);
-
-      ctx.drawImage(
-        image,
-        (layerSize.width - image.naturalWidth) / 2,
-        (layerSize.height - image.naturalHeight) / 2,
-      );
-    }
-
-    await fs.writeFile(`./output/${i + 1}.png`, canvas.toBuffer());
-  }
+  const imageCombinations = cartesian(...layerImages);
+  console.log(`Generating ${imageCombinations.length} images...`);
+  await generateImages(imageCombinations, layerSize);
+  console.log('Done.');
 }
 
 async function getLayerDirectories() {
-  const dirent = await fs.readdir('./layers', {withFileTypes: true});
+  const dirent = await readdir('./layers', {withFileTypes: true});
 
   return dirent.reduce((entries, entry) => {
     if (entry.isDirectory()) {
@@ -57,7 +45,7 @@ async function getLayerDirectories() {
 }
 
 async function getLayerOrder() {
-  const yml = await fs.readFile('./config.yml', {encoding: 'utf8'});
+  const yml = await readFile('./config.yml', {encoding: 'utf8'});
   const config = parse(yml) as {order: string[]};
   return config.order.map(dir => dir.trim());
 }
@@ -68,22 +56,20 @@ async function getLayerImages(orderedDirs: string[]) {
 }
 
 async function getLayerSize(layerImages: string[][]) {
-  const layerSize = {width: 0, height: 0};
+  const layerSize = new LayerSize();
 
   await Promise.all(
     layerImages.map(layer =>
       Promise.all(
         layer.map(async image => {
-          const size = await sizeOf(image);
+          const size = (await sizeOf(image)) as LayerSize;
 
-          if (size && size.width && size.height) {
-            if (layerSize.width < size.width) {
-              layerSize.width = size.width;
-            }
+          if (layerSize.width < size.width) {
+            layerSize.width = size.width;
+          }
 
-            if (layerSize.height < size.height) {
-              layerSize.height = size.height;
-            }
+          if (layerSize.height < size.height) {
+            layerSize.height = size.height;
           }
         }),
       ),
@@ -93,8 +79,29 @@ async function getLayerSize(layerImages: string[][]) {
   return layerSize;
 }
 
-function generateImageCombinations(layerImages: string[][]): string[][] {
-  return [...cartesian(...layerImages)];
+async function generateImages(imageCombinations: string[][], layerSize: LayerSize, index = 0) {
+  const imageCombinationsBatch = imageCombinations.slice(index, index + BATCH_SIZE);
+
+  if (imageCombinationsBatch.length > 0) {
+    console.log(`Batch: ${index / BATCH_SIZE + 1} / ${Math.ceil(imageCombinations.length / BATCH_SIZE)}`);
+
+    await Promise.all(
+      imageCombinationsBatch.map((imageCombination, i) =>
+        sharp({
+          create: {
+            width: layerSize.width,
+            height: layerSize.height,
+            channels: 4,
+            background: {r: 0, g: 0, b: 0, alpha: 0},
+          },
+        })
+          .composite(imageCombination.map(image => ({input: image})))
+          .toFile(`./output/${i + index + 1}.png`),
+      ),
+    );
+
+    await generateImages(imageCombinations, layerSize, index + BATCH_SIZE);
+  }
 }
 
 main();
--- lib.ts
+++ lib.ts
@@ -1,6 +1,14 @@
-export function* cartesian(...list: any[]): any {
+export class LayerSize {
+  constructor(public width = 0, public height = 0) {}
+}
+
+function* _cartesian<T>(...list: T[][]): Generator<T[]> {
   const head = list[0];
   const tail = list.slice(1);
-  const remainder = tail.length ? cartesian(...tail) : [[]];
+  const remainder = tail.length ? _cartesian(...tail) : ([[]] as T[][]);
   for (const r of remainder) for (const h of head) yield [h, ...r];
+}
+
+export function cartesian<T>(...list: T[][]) {
+  return [..._cartesian(...list)];
 }
